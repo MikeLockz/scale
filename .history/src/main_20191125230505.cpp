d281e1854a5991eede7be6c34bf7aef828b4c0be
Mike Lockwitz <mlockwitz@gmail.com>
2019-11-25 23:20:53 -0700

    Initial commit

---
+++ .history/src/main_20191125230505.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+#define BUILTIN_LED 2
+
+void setup()
+{
+  // put your setup code here, to run once:
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+  digitalWrite(BUILTIN_LED, LOW);
+  delay(500);
+  digitalWrite(BUILTIN_LED, HIGH);
+  delay(500);
+}
---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+#include "HX711.h"
+
+#define BUILTIN_LED 2
+
+void setup()
+{
+  // put your setup code here, to run once:
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+  digitalWrite(BUILTIN_LED, LOW);
+  delay(500);
+  digitalWrite(BUILTIN_LED, HIGH);
+  delay(500);
+}
